519818d46e75adbbd04257c5660f86d55c292771
Patrick <patrick@godknows.de>
2022-11-09 15:53:42 +0100

    New Models

---
+++ Models/Character.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,9 @@
+namespace dotnet_rpg.Models
+{
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
