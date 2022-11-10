189fb5640be57596d2aa2c56cfe08b2db876a831
Patrick <patrick@godknows.de>
2022-11-10 11:43:47 +0100

    Add a New Character with POST

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -27,5 +27,12 @@ namespace dotnet_rpg.Controllers
         {
             return Ok(characters.FirstOrDefault(c => c.Id == id));
         }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
     }
 }
