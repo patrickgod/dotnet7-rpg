e620ca9f1513606881b31903adf2f07e97a8c5e1
Patrick <patrick@godknows.de>
2022-11-10 11:27:43 +0100

    First Steps with Attribute Routing

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -11,12 +11,21 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character { Name = "Sam" }
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
 
         [HttpGet]
-        public ActionResult<Character> Get()
+        public ActionResult<Character> GetSingle()
         {
-            return Ok(knight);
+            return Ok(characters[0]);
         }
     }
 }
