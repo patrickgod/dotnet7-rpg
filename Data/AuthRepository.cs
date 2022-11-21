c4f419780b0eefcd288265dd00adc7e8fc353624
Patrick <patrick@godknows.de>
2022-11-21 21:38:08 +0100

    User Registration

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -7,19 +7,44 @@ namespace dotnet_rpg.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+            
+        }
+
         public Task<ServiceResponse<string>> Login(string username, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            var response = new ServiceResponse<int>();
+            response.Data = user.Id;
+            return response;
         }
 
         public Task<bool> UserExists(string username)
         {
             throw new NotImplementedException();
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
