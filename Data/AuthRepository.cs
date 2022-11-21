7e39763abefc911d43b40856804adf0f3d1d8604
Patrick <patrick@godknows.de>
2022-11-21 21:27:46 +0100

    Authentication Repository

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
