8c2cbddb256c94a103c3cceba80eb7c87054947d
Patrick <patrick@godknows.de>
2022-11-22 22:48:56 +0100

    Proper User-Character Relationship - Add Character

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -27,12 +27,16 @@ namespace dotnet_rpg.Services.CharacterService
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
             var character = _mapper.Map<Character>(newCharacter);
+            character.User = await _context.Users.FirstOrDefaultAsync(u => u.Id == GetUserId());
             
             _context.Characters.Add(character);
             await _context.SaveChangesAsync();
 
             serviceResponse.Data =
-                await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
+                await _context.Characters
+                    .Where(c => c.User!.Id == GetUserId())
+                    .Select(c => _mapper.Map<GetCharacterDto>(c))
+                    .ToListAsync();
             return serviceResponse;
         }
 
