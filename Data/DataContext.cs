8abf888c768afb866b3b06cc6875b9226c14d505
Patrick <patrick@godknows.de>
2022-11-23 00:41:11 +0100

    Data Seeding - Add Skills to the Database

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -9,7 +9,16 @@ namespace dotnet_rpg.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
-            
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Skill>().HasData(
+                new Skill { Id = 1, Name = "Fireball", Damage = 30 },
+                new Skill { Id = 2, Name = "Frenzy", Damage = 20 },
+                new Skill { Id = 3, Name = "Blizzard", Damage = 50 }
+            );
         }
 
         public DbSet<Character> Characters => Set<Character>();
