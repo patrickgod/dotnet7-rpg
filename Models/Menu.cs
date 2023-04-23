f8dc00e4ad0fad18a4e09f3a63bc9c2633ce4369
lidiami8 <lidiami8@gmail.com>
2023-04-23 17:59:08 +0200

    creo modello menu nel folder Models

---
+++ Models/Menu.cs
@@ -0,0 +1,28 @@
+﻿namespace dotnet_rpg.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string AppName { get; set; }
+        public string Logo { get; set; }
+        public List<parmItem> dbnames { get; set; }
+        public string Language { get; set; }
+        public string FullName { get; set; }
+        public partial class parmItem
+        {
+            public string cod_APPL { get; set; }
+
+            public string APP_NAME { get; set; }
+        }
+        public partial class MenuItem
+        {
+            public int id { get; set; }
+            public object? ParentID { get; set; }
+            public string Title { get; set; }
+            public string Url { get; set; }
+
+        }
+
+
+    }
+}
