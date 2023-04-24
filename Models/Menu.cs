b87c963f647d1abb094072ae15e4b4d1ab83de77
lidiami8 <lidiami8@gmail.com>
2023-04-24 07:04:55 +0200

    Ho dovuto commentare la classe menu.cs perchè non funziona enum DbNames (analogo di RpgClass di Character) e poi non so come gestire le classi parziale del modello PFNG

--- Models/Menu.cs
+++ Models/Menu.cs
@@ -1,28 +1,29 @@
-﻿namespace dotnet_rpg.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace dotnet_rpg.Models
 {
     public class Menu
     {
         public int Id { get; set; }
-        public string AppName { get; set; }
-        public string Logo { get; set; }
-        public List<parmItem> dbnames { get; set; }
-        public string Language { get; set; }
-        public string FullName { get; set; }
-        public partial class parmItem
-        {
-            public string cod_APPL { get; set; }
+        public string AppName { get; set; } = "nomedellapp";
+        public string Logo { get; set; } = "stringalogo";
+        //public List<ParmItem>? Dbnames { get; set; }
+       // public DbNames class { get; set; } = DbNames.Grc;
+    // public string? Language { get; set; }
+    //public string? FullName { get; set; }
+    //public partial class ParmItem
+    //{
+    //    public string? Cod_APPL { get; set; }
 
-            public string APP_NAME { get; set; }
-        }
-        public partial class MenuItem
-        {
-            public int id { get; set; }
-            public object? ParentID { get; set; }
-            public string Title { get; set; }
-            public string Url { get; set; }
+    //    public string? APP_NAME { get; set; }
+    //}
+    //public partial class MenuItem
+    //{
+    //    public int Id { get; set; }
+    //    public object? ParentID { get; set; }
+    //    public string? Title { get; set; }
+    //    public string? Url { get; set; }
 
-        }
-
-
-    }
-}
+    }}
